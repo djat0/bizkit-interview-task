987918fe632998df6e11a7d7c6a701234c8b58b9
djat0 <djatology@gmail.com>
2023-04-15 15:01:50 +0800

    modified search.py and match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,5 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    #representing each list as set and using issubset() function to check if fave_numbers_2 is subset of fave_numbers_1
+    return (set(fave_numbers_2).issubset(set(fave_numbers_1)))
--- phasebook/search.py
+++ phasebook/search.py
@@ -10,6 +10,26 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 def search():
     return search_users(request.args.to_dict()), 200
 
+"""
+The has_match function will check if an item(user) has a match from the parameter(args). by default set to False and if a match (key, value) pair is found will be changed to True. used switch statement to implement matching requirements per key(field)
+"""
+def has_match(args, user):
+    matched = False
+    for key, value in args.items():
+        match key:
+            case "id":
+                if value == user['id']:
+                    matched = True
+            case "name":
+                if value.lower() in user['name'].lower():
+                    matched = True
+            case "age":
+                if (user['age'] - 1) == int(value) or (user['age'] + 1) == int(value) or user['age'] == int(value):
+                    matched = True
+            case "occupation":
+                if value.lower() in user['occupation'].lower():
+                    matched = True
+    return matched
 
 def search_users(args):
     """Search users database
@@ -27,4 +47,12 @@ def search_users(args):
 
     # Implement search here!
 
-    return USERS
+    #checks if args is empty and return the full USERS list
+    if not args :
+        return USERS
+    else:
+        #create an new list based on the items that matched the parameters using has_match() function
+        result = [user for user in USERS if has_match(args, user)]
+
+    return result
+
